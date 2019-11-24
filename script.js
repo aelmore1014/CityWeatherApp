de134c232219f415964ac664c38b3b7a4868190a
Alex Elmore <alexelmore1014@gmail.com>
2019-11-23 23:46:17 -0500

    fixed some java problems

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ var APIKey = "c098ebad01cb430089c372ffb1e6503b";
 
 $("#submitCity").on('click', function (event) {
     var cityInput = $("#weatherInput").val().trim();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=" + cityInput + "&units=imperial&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + cityInput + "&units=imperial&appid=" + APIKey;
     event.preventDefault()
     console.log(cityInput);
 
@@ -16,15 +15,20 @@ method:"GET"
 console.log(response);
 
 var cityName =  response.name;
-// var date = $("<h2>").text().append(cityName);
+var icon = response.weather[0].icon;
+var createIcon = $("<img class='weatherIcon' src=http://www.openweathermap.org/img/wn/" + icon + ".png alt='Weather Icon'>");
 var temperature = response.main.temp;
 var humidity = response.main.humidity;
 var windSpeed = response.wind.speed;
 
 $("#weatherDiv").append("City: " + cityName);
+$("#weatherDiv").append(createIcon);
 $("#weatherDiv2").append("Wind Speed: " + windSpeed + "MPH <br>");
 $("#weatherDiv2").append("Temperature: " + temperature + "°F <br>");
 $("#weatherDiv2").append("Humidity: " + humidity + "%");
+
+
+
 
 
 });
